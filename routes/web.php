c6834bc66e36d2e3557c5c7c79c89b1ef191b535
Mundo <alondra.hernandez5404@alumnos.udg.mx>
2025-11-28 09:30:37 -0600

    Agregue codigo a web.php

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-use Laravel\Fortify\Features;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
-})->name('home');
+    return view('welcome');
+})->name('inicio');
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
+Route::get('/dashboard', function () {
+    return Inertia\Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/settings.php';
+
+use App\Http\Controllers\UsuariosPublicosController;
+use App\Http\Controllers\DesarrolladoresController;
+
