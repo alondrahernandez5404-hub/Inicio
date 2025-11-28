5aff90e1c9aa741dd971edc3952d8f2d882d53de
Mundo <alondra.hernandez5404@alumnos.udg.mx>
2025-11-28 10:59:11 -0600

    Hice y/o modifique modelos de usuario y desarrollador

---
+++ app/Models/Desarrollador.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Desarrollador extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nombre_completo',
+        'clave',
+        'telefono',
+        'cp',
+        'pais',
+        'password'
+    ];
+}
