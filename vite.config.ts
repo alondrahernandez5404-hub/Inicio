cab81519cb0456af177ba38ce7308fb5342a8429
Mundo <alondra.hernandez5404@alumnos.udg.mx>
2025-11-30 11:11:42 -0600

    Solucione el problema de app.css + tailwild, al fiinn

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import tailwindcss from '@tailwindcss/vite';
 import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 
 export default defineConfig({
   plugins: [
     laravel({
-      input: ['resources/js/app.ts'], 
+      input: ['resources/css/app.css', 'resources/js/app.ts'],
       ssr: 'resources/js/ssr.ts',
       refresh: true,
     }),
-    tailwindcss(),
     wayfinder({
       formVariants: true,
     }),
